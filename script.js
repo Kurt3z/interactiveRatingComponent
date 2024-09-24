7a5c79eeac337bdc604de9ce5d3c17079f1a83b1
Tiago Pinto <tiago.webdev10@gmail.com>
2024-09-24 18:55:08 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+// ELEMENTS
+const ratingElements = document.querySelectorAll(".value");
+const questionSectionEl = document.querySelector(".question-section");
+const thankYouSectionEl = document.querySelector(".thank-you--section");
+const selectedValueEl = document.querySelector(".selected-value");
+console.log(questionSectionEl);
+console.log(thankYouSectionEl);
+
+// BUTTON
+const btnSubmit = document.querySelector(".btn-submit");
+
+let selectedRating;
+
+ratingElements.forEach(function (rating) {
+  rating.addEventListener("click", function (e) {
+    for (const element of ratingElements) {
+      if (element === e.target) {
+        element.classList.add("background-orange");
+        selectedRating = Number(element.textContent);
+        console.log(selectedRating);
+      } else element.classList.remove("background-orange");
+    }
+  });
+});
+
+btnSubmit.addEventListener("click", function () {
+  questionSectionEl.classList.add("hidden");
+  thankYouSectionEl.classList.remove("hidden");
+  selectedValueEl.textContent = selectedRating;
+});
